df822495f86727bbf090d58046d06b2884f2e17b
nepeat <nepeat@users.noreply.github.com>
2016-12-04 03:52:52 -0900

    Egg inside antispam.

--- bot/nepeatbot/plugins/plugin_fun.py
+++ bot/nepeatbot/plugins/plugin_fun.py
@@ -1,3 +1,4 @@
+import discord
 from nepeatbot.plugins.common import PluginBase, command
 
 class FunPlugin(PluginBase):
@@ -5,3 +6,13 @@ class FunPlugin(PluginBase):
     async def fart(self, channel):
         print("FARTED")
         await self.bot.send_message(channel, "\N{DASH SYMBOL}")
+
+    @command("egg")
+    async def egg(self, message):
+        em = discord.Embed(title='EGG EGG EG', description='EGGEGEGEGEGEGEG.', colour=0x416600)
+        em.set_author(name='EGGEGG', icon_url="https://i.imgur.com/yuesnwm.jpg")
+        em.set_footer(text="Egg.", icon_url="https://i.imgur.com/7msZamU.png")
+        em.add_field(name="EGG", value="ROLLS")
+        em.add_field(name="ROLL", value="EGG")
+        em.set_image(url="https://i.imgur.com/YFeZaoM.jpg")
+        await self.bot.send_message(message.channel, embed=em)
