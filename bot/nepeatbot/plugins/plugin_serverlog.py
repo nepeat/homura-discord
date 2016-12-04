d8bbcae61fb6f497a95b367ac4a060e4f64f15f4
nepeat <nepeat@users.noreply.github.com>
2016-12-04 03:53:02 -0900

    Fix permissions for !undelete.

--- bot/nepeatbot/plugins/plugin_serverlog.py
+++ bot/nepeatbot/plugins/plugin_serverlog.py
@@ -14,7 +14,7 @@ class ServerLogPlugin(PluginBase):
         super().__init__(*args, **kwargs)
         self.events_url = os.environ.get("BOT_WEB", "http://localhost:5000")
 
-    @command("undelete")
+    @command("undelete", requires_admin=True)
     async def cmd_undelete(self, message, bot):
         messages = await self.get_events("delete", message.server, message.channel)
         if not messages:
