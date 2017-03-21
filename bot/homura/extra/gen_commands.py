1dc280391f8a5952679775ad974d097c52052013
nepeat <nepeat@users.noreply.github.com>
2017-03-20 18:02:34 -0800

    Unify generation of the plugins/permissions help pages

---
+++ bot/homura/extra/gen_commands.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+import sys
+import os
+import json
+import io
+import yaml
+import operator
+
+from homura import NepeatBot
+
+# Argument parsing
+
+try:
+    export_type = sys.argv[1]
+    if export_type.lower() not in ["json", "yaml"]:
+        raise ValueError(f"{export_type} is not a valid extension.")
+except IndexError:
+    export_type = "json"
+except ValueError as e:
+    print(str(e))
+    sys.exit(os.EX_USAGE)
+
+# Data generation
+
+bot = NepeatBot()
+
+def escape(text):
+    return text.replace("|", "\|")
+
+output = {}
+
+for plugin in bot.plugins:
+    if plugin.owner_only:
+        continue
+
+    plugin_name = plugin.__class__.__name__.replace("Plugin", "")
+    if plugin_name not in output:
+        output[plugin_name] = {"commands": []}
+
+    for command_name, command_func in sorted(plugin.commands.items()):
+        name = command_func.info["name"]
+        description = command_func.info["description"]
+
+        output[plugin_name]["commands"].append({
+            "name": escape(name),
+            "description": escape(description),
+            "permission": command_func.info.get("permission")
+        })
+
+    # bad
+    output[plugin_name]["commands"] = sorted(output[plugin_name]["commands"], key=operator.itemgetter("name"))
+
+# Data output
+
+stdout = io.StringIO()
+
+if export_type == "yaml":
+    yaml.dump(output, stdout, indent=4, default_flow_style=False)
+else:
+    json.dump(output, stdout, indent=4)
+
+stdout.seek(0)
+print(stdout.read())
