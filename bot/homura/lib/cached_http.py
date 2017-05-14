0ef6d4c82160618860fe6293c3b2360b76bcc8c1
nepeat <nepeat@users.noreply.github.com>
2017-05-14 01:54:42 -0800

    Docstrings for cached HTTP

--- bot/homura/lib/cached_http.py
+++ bot/homura/lib/cached_http.py
@@ -6,17 +6,28 @@ import urllib.parse
 from homura.util import md5_string
 import json
 
-CACHE_TIME = 60 * 5 # 5 minutes
 USER_AGENT = "github.com/nepeat/homura-discord | nepeat#6071 | This is discord bot. This is mistake."
 
 
 class CachedHTTP(object):
-    def __init__(self, bot):
+    def __init__(self, bot, default_cache_time=300):
         self.bot = bot
+        # 60 seconds * 5 minutes = 300 seconds
+        self.cache_time = 300
 
-    def generate_cachekey(self, url: str, params: dict):
+    def generate_cachekey(self, url: str, params: dict, **kwargs) -> str:
+        """
+        Generates a cache key for Redis storage.
+
+        :param url: URL for the base portion of the key.
+        :param params: Request parameters that will be hashed into the key.
+        :param kwargs: `json` is the only argument that modifies the key.
+        """
         pieces = urllib.parse.urlparse(url)
         cache_key = pieces.netloc + ":" + md5_string(pieces.path)
+
+        if kwargs.get("json", True):
+            cache_key += ":json"
 
         cache_extra = ":".join([str(x) + ":" + str(y) for x, y  in params.items()])
         if cache_extra:
@@ -24,9 +35,19 @@ class CachedHTTP(object):
 
         return cache_key
 
-    async def get(self, url, params={}, headers=None, asjson=False):
-        cache_key = self.generate_cachekey(url, params)
+    async def get(self, url, params={}, headers=None, asjson=False, **kwargs):
+        """
+        Extremely simplified cached GET for external APIs.
+
+        :param url: URL of the external API
+        :param params: URL parameters for the API
+        :param headers: Additional headers to be sent to the API.
+        :param asjson: Returns JSON
+        :param kwargs: `json` is the only argument, that returns the response as JSON.
+        """
+        cache_key = self.generate_cachekey(url, params, json=asjson)
         cache_data = await self.bot.redis.get(cache_key)
+        cache_time = kwargs.get("cache_time", self.cache_time)
 
         if cache_data:
             return json.loads(cache_data)
@@ -47,5 +68,5 @@ class CachedHTTP(object):
                 reply = await response.text()
 
 
-        await self.bot.redis.setex(cache_key, CACHE_TIME, json.dumps(reply))
+        await self.bot.redis.setex(cache_key, cache_time, json.dumps(reply))
         return reply
